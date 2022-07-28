e7c3acf6ddd3b254eec1578d995543afe76508e7
iq136boy <dmfeng8898@gmail.com>
2022-07-28 12:45:35 -0500

    fix

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -678,7 +678,7 @@ def compile()
   }
   def s_id = runsql("select id from session where reason=${params.job_label}")
     // Run the jobs on the cluster
-  sh "srun --no-kill -p ${slurm_partition} -N 1-10 -l bash -c 'docker run ${docker_args} ${tuna_docker_name} python3 /tuna/tuna/go_fish.py --local_machine ${compile_cmd} -l ${params.job_label} --session_id=${s_id}'"
+  sh "srun --no-kill -p ${slurm_partition} -N 1-10 -l bash -c 'docker run ${docker_args} ${tuna_docker_name} python3 /tuna/tuna/go_fish.py --local_machine ${compile_cmd} -l ${params.job_label} --session_id ${s_id}'"
 }
 
 
