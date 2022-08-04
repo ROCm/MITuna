e4c5871ca9cea22a3c29fd30a587863be90359f1
Reid Kawaja <reid.kawaja@amd.com>
2022-08-04 18:44:14 -0500

    bash scripts

---
+++ tests/covscripts/coverage_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+python3 -m coverage run -m pytest #runs coverage reports 
+python3 -m coverage json #exports coverage reports into a JSON file 
+mv coverage.json ../MITunaX/tests/covscripts/buffer #move file into covscripts/buffer folder
+python3 tests/covscripts/parse_attributes.py #parse coverage from JSON file and saves it into buffer file 
+file="../MITunaX/tests/covscripts/buffer/coverage_percentage.txt" #picks up the file with the coverage percentage
+name=$(cat "$file")        #assigns the output from the file 
+echo $name   #testing that the variable is correct 
