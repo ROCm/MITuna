e48a524fdd6365c6bef3a7080ae61101129ccad9
Reid Kawaja <reid.kawaja@amd.com>
2022-08-05 15:18:58 -0500

    jenkin fixes

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -517,8 +517,7 @@ def runCodeCov() {
           sh "mv coverage.json ../MITunaX/tests/covscripts/buffer"
           sh "python3 tests/covscripts/parse_attributes.py"
           sh "file= ../MITunaX/tests/covscripts/buffer/coverage_percentage.txt"
-          sh "name= ${(cat "$file")}"
-          myvar = sh "echo ${name}"
+          myvar = sh "\$(cat "\$file")"
           CODE_COV = 10
           if (CODE_COV > myvar) {
           error "Not added to env: ${item}"
