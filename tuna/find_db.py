3f5936cb28ae3df0d106e5eccaddd4b8a7002733
Christopher Erb <chris.erb65@gmail.com>
2022-08-12 13:14:45 -0500

    pylint

--- tuna/find_db.py
+++ tuna/find_db.py
@@ -26,7 +26,7 @@
 ###############################################################################
 """find db class"""
 from sqlalchemy import Column, Integer, String, UniqueConstraint, ForeignKey, orm
-from sqlalchemy import Float, BigInteger, Boolean, and_, Text
+from sqlalchemy import Float, BigInteger, Boolean, Text
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declared_attr
 
