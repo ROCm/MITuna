706eff25852c7ab1503726f803d9e99f50f5dacd
Christopher Erb <chris.erb65@gmail.com>
2022-08-12 13:08:49 -0500

    yapf

--- tuna/find_db.py
+++ tuna/find_db.py
@@ -65,11 +65,10 @@ class FindDBMixin():  # pylint: disable=too-many-instance-attributes
 
     # CE: Solver applicability can change between miopen versions
     # find if this fdb entry is currently applicable
-    query = sess.query(fdb_obj).filter(
-        fdb_obj.session == session_id,
-        fdb_obj.config == self.config, 
-        fdb_obj.opencl == self.opencl,
-        fdb_obj.valid == 1)
+    query = sess.query(fdb_obj).filter(fdb_obj.session == session_id,
+                                       fdb_obj.config == self.config,
+                                       fdb_obj.opencl == self.opencl,
+                                       fdb_obj.valid == 1)
 
     if self.solver:
       query = query.filter(fdb_obj.solver == self.solver)
