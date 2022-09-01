58dc434bb1adcfc73236e268ebff05b35aa0ec00
Christopher Erb <chris.erb65@gmail.com>
2022-09-01 13:42:07 -0500

    test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -115,9 +115,9 @@ def finApplicability(){
         env.OTEL_PYTHON_DISABLED_INSTRUMENTATIONS="pymysql"
         env.OTEL_LOG_LEVEL="debug"
 
-        sh "./tuna/go_fish.py --init_session -l new_session --arch 'gfx908' --num_cu 120"
+        sh "./tuna/go_fish.py --init_session -l new_session --arch gfx908 --num_cu 120"
         def sesh1 = 1 //runsql("select id from session order by id asc limit 1")
-        sh "./tuna/go_fish.py --init_session -l new_session2 --arch 'gfx908' --num_cu 120"
+        sh "./tuna/go_fish.py --init_session -l new_session2 --arch gfx908 --num_cu 120"
         def sesh2 = 2 //runsql("select id from session order by id desc limit 1")
 
         sh "./tuna/import_configs.py -t recurrent_${branch_id} --mark_recurrent -f utils/recurrent_cfgs/alexnet_4jobs.txt"
