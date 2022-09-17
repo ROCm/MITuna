12782ad9f8c89c9e296c5d0bdd3ef343dc520184
Christopher Erb <chris.erb65@gmail.com>
2022-09-17 03:04:36 -0500

    debug

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -666,6 +666,7 @@ def compile()
   }
   //def s_id = runsql("select id from session where reason='${params.job_label}'")
     // Run the jobs on the cluster
+  sh "echo sanity check session ${params.session_id} close"
   sh "srun --no-kill -p ${slurm_partition} -N 1-10 -l bash -c 'docker run ${docker_args} ${tuna_docker_name} python3 /tuna/tuna/go_fish.py ${compile_cmd} --session_id ${params.session_id}'"
 }
 
