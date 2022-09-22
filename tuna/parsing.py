e17fa23e8f3fc358b5f29441b33d4d9b86e0ea03
Reid Kawaja <reid.kawaja@amd.com>
2022-09-22 13:35:43 -0500

    line [1/3]: function

--- tuna/parsing.py
+++ tuna/parsing.py
@@ -377,15 +377,22 @@ def arg_valid(arg, val):
   return True
 
 
-def parse_driver_line(line):
-  """return network parameters from driver invocation"""
-  fds = {}
+def parse_line(line):
+  """parse the lines from MIopenDriver"""
   line = line.strip()
   start = line.find('MIOpenDriver')
   if start == -1:
     raise ValueError(f"Invalid driver commmand line: '{line}'")
 
   line = line[start:]
+
+  return line
+
+
+def parse_driver_line(line):
+  """return network parameters from driver invocation"""
+  fds = {}
+  line = parse_line(line)
   direction = None
   if line.find('-F') != -1:
     direction = line.partition("-F")[2][:2].strip()
@@ -396,6 +403,7 @@ def parse_driver_line(line):
 
   fds = compose_fds(fds, tok, line)
   return fds, direction
+
 
 
 def compose_fds(fds, tok, line):
