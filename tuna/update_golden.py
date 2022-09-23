bc5762d10677988aa6cf01babd3f4b08fa720a48
Christopher Erb <chris.erb65@gmail.com>
2022-09-23 09:55:23 -0500

    reviews

--- tuna/update_golden.py
+++ tuna/update_golden.py
@@ -224,7 +224,7 @@ def main():
   query = get_fdb_query(dbt)
   add, err = merge_golden_entries(dbt, args.golden_v, query.all())
 
-  print(f"Merged: {add}, Errored: {err}")
+  LOGGER.info(f"Merged: {add}, Errored: {err}")
 
 
 if __name__ == '__main__':
