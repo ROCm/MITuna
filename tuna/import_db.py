0a72950d9a538ef24a8337a79b4085ce984a69de
Christopher Erb <christopher.erb@amd.com>
2022-09-28 01:36:41 +0000

    add fdb import

--- tuna/import_db.py
+++ tuna/import_db.py
@@ -25,6 +25,7 @@
 #
 ###############################################################################
 """script for merging find db or perf db files, across machines or locally"""
+import os
 import sqlite3
 
 from tuna.dbBase.sql_alchemy import DbSession
@@ -39,8 +40,11 @@ from tuna.driver_conv import DriverConvolution
 from tuna.import_configs import insert_config
 from tuna.metadata import PREC_TO_CMD
 from tuna.metadata import get_solver_ids
+from tuna.parsing import parse_fdb_line
 
 LOGGER = setup_logger('import_db')
+
+COMMIT_FREQ = 1000
 
 
 def parse_args():
@@ -75,6 +79,68 @@ def parse_args():
   return args
 
 
+def get_cfg_driver(sqlite_cfg):
+  """Takes in a dict containing a sqlite config row
+  and returns convolution driver object"""
+  mysql_cfg = sqlite_to_mysql_cfg(sqlite_cfg)
+
+  #constructing a conv_config entry (dict)
+  mysql_cfg['in_layout'] = mysql_cfg['layout']
+  mysql_cfg['out_layout'] = mysql_cfg['layout']
+  mysql_cfg['fil_layout'] = mysql_cfg['layout']
+  cnv_cmd = PREC_TO_CMD.get(ConfigType.convolution).get(mysql_cfg['data_type'])
+
+  driver = DriverConvolution('', cmd=cnv_cmd, kwargs=mysql_cfg)
+
+  return driver
+
+
+def get_sql_cfg_id_map(dbt, cnx, args):
+  """ load configs into db from sqlite, create mapping to mysql configs """
+  counts = {}
+  counts['cnt_configs'] = 0
+  counts['cnt_tagged_configs'] = set()
+
+  cfg_filter = None
+  config_rows, config_cols = get_sqlite_data(cnx, 'config', cfg_filter)
+
+  total = len(config_rows)
+  cvrt = {}  #update config id to inserted location
+  LOGGER.info("Insert Configurations")
+  for i, cfg_row in enumerate(config_rows):
+    sqlite_cfg = dict(zip(config_cols, cfg_row))
+    sqlite_id = sqlite_cfg['id']
+    sqlite_cfg = valid_cfg_dims(sqlite_cfg)
+
+    driver = get_cfg_driver(sqlite_cfg)
+    ins_id = insert_config(driver, counts, dbt, args)
+    cvrt[sqlite_id] = ins_id
+
+    if (total / 10) % (i + 1) == 0:
+      LOGGER.info("Config Insert %s: %s", i, cfg_row)
+
+  return cvrt
+
+
+def set_fdb_data(fdb_entry, fdb_key, alg_lib, workspace, kernel_time):
+  fdb_entry.fdb_key = fdb_key
+  fdb_entry.alg_lib = alg_lib
+  fdb_entry.workspace_sz = workspace
+  fdb_entry.kernel_time = kernel_time
+  fdb_entry.kernel_group = fdb_entry.id
+
+  if not fdb_entry.params:
+    fdb_entry.params = '' 
+
+
+def set_pdb_data(fdb_entry, params):
+  fdb_entry.params = params
+  if not fdb_entry.kernel_time:
+    fdb_entry.kernel_time = -1 
+  if not fdb_entry.workspace_sz:
+    fdb_entry.workspace_sz = -1 
+
+
 def get_fdb_entry(session, dbt, config, solver, ocl):
   fdb_entry = dbt.find_db_table()
   fdb_entry.config = config
@@ -95,28 +161,12 @@ def get_fdb_entry(session, dbt, config, solver, ocl):
   return fdb_entry
 
 
-def set_fdb_data(fdb_entry, fdb_key, alg_lib, workspace, kernel_time):
-  fdb_entry.fdb_key = fdb_key
-  fdb_entry.alg_lib = alg_lib
-  fdb_entry.workspace_sz = workspace
-  fdb_entry.kernel_time = kernel_time
-  fdb_entry.kernel_group = fdb_entry.id
-
-
-def set_pdb_data(fdb_entry, params):
-  fdb_entry.params = params
-  if not fdb_entry.kernel_time:
-    fdb_entry.kernel_time = -1 
-  if not fdb_entry.workspace_sz:
-    fdb_entry.workspace_sz = -1 
-
-
 def insert_perf_db(dbt, perf_rows, perf_cols, cvrt):
   """insert sqlite perf_db table into mysql perf_db"""
   insert_ids = []
   solver_id_map, _ = get_solver_ids()
   with DbSession() as session:
-    for row in perf_rows:
+    for i, row in enumerate(perf_rows):
       entry = dict(zip(perf_cols, row))
       cfg_id = cvrt[entry['config']]
       slv_id = solver_id_map[entry['solver']]
@@ -127,33 +177,18 @@ def insert_perf_db(dbt, perf_rows, perf_cols, cvrt):
       fdb_entry.valid = True
       insert_ids.append(fdb_entry.id)
 
+      if len(insert_ids) % COMMIT_FREQ == 0:
+        session.commit()
+
     session.commit()
 
   return insert_ids
 
 
-def record_perfdb(args, dbt):  #pylint: disable=too-many-locals
+def record_perfdb(dbt, args):  #pylint: disable=too-many-locals
   """insert perf_db entry from sqlite file to mysql"""
   cnx = sqlite3.connect(args.target_file)
-  counts = {}
-  counts['cnt_configs'] = 0
-  counts['cnt_tagged_configs'] = set()
-
-  cfg_filter = None
-  config_rows, config_cols = get_sqlite_data(cnx, 'config', cfg_filter)
-
-  total = len(config_rows)
-  cvrt = {}  #update config id to inserted location
-  LOGGER.info("Insert Configurations")
-  for i, cfg_row in enumerate(config_rows):
-    sqlite_cfg = dict(zip(config_cols, cfg_row))
-    sqlite_id = sqlite_cfg['id']
-    sqlite_cfg = valid_cfg_dims(sqlite_cfg)
-    driver = get_cfg_driver(sqlite_cfg)
-    ins_id = insert_config(driver, counts, dbt, args)
-    cvrt[sqlite_id] = ins_id
-    if (total / 10) % (i + 1) == 0:
-      LOGGER.info("Config Insert %s: %s", i, cfg_row)
+  cvrt = get_sql_cfg_id_map(dbt, cnx, args)
 
   perf_rows, perf_cols = get_sqlite_data(
       cnx, 'perf_db', {'config': [cfg_id for cfg_id in cvrt.keys()]})
@@ -163,20 +198,30 @@ def record_perfdb(args, dbt):  #pylint: disable=too-many-locals
   insert_perf_db(dbt, perf_rows, perf_cols, cvrt)
 
 
-def get_cfg_driver(sqlite_cfg):
-  """Takes in a dict containing a sqlite config row
-  and returns convolution driver object"""
-  mysql_cfg = sqlite_to_mysql_cfg(sqlite_cfg)
+def record_fdb(dbt, args):
+  counts = {}
+  counts['cnt_configs'] = 0
+  counts['cnt_tagged_configs'] = set()
+  solver_id_map, _ = get_solver_ids()
+  with open(os.path.expanduser(args.target_file), "r") as infile:  # pylint: disable=unspecified-encoding
+    with DbSession() as session:
+      num_line = 0
+      for line in infile:
+        driver = DriverConvolution(line)
+        ins_id = insert_config(driver, counts, dbt, args)
+      
+        fdb_data = parse_fdb_line(line)
+        for fdb_key, algs in fdb_data.items():
+          for alg in algs:
+            slv_id = solver_id_map[alg['solver']]
+            fdb_entry = get_fdb_entry(session, dbt, ins_id, slv_id, False)
+            set_fdb_data(fdb_entry, fdb_key, alg['alg_lib'], alg['workspace_sz'], alg['kernel_time'])
 
-  #constructing a conv_config entry (dict)
-  mysql_cfg['in_layout'] = mysql_cfg['layout']
-  mysql_cfg['out_layout'] = mysql_cfg['layout']
-  mysql_cfg['fil_layout'] = mysql_cfg['layout']
-  cnv_cmd = PREC_TO_CMD.get(ConfigType.convolution).get(mysql_cfg['data_type'])
+        num_line += 1
+        if num_line % COMMIT_FREQ == 0:
+          session.commit()
 
-  driver = DriverConvolution('', cmd=cnv_cmd, kwargs=mysql_cfg)
-
-  return driver
+      session.commit()
 
 
 def print_sqlite_rows(cnx, cfg_filter):
@@ -204,8 +249,10 @@ def main():
   if not args.session_id:
     args.session_id = Session().add_new_session(args, None)
   dbt = DBTables(session_id=args.session_id)
-
-  record_perfdb(args, dbt)
+  if args.target_file.endswith(".db"):
+    record_perfdb(dbt, args)
+  elif args.target_file.endswith(".fdb.txt"):
+    record_fdb(dbt, args)
 
 
 if __name__ == '__main__':
