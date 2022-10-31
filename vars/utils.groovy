e136d3969807104b9cd8501f1950d1a68da33b96
Muhammad Saud Ul Hassan <muhammad.saud.ms@gmail.com>
2022-10-31 11:15:36 -0500

    debugging...

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -330,6 +330,7 @@ def solverAnalyticsTest(){
         env.PYTHONPATH = env.WORKSPACE
         env.PATH = "${env.WORKSPACE}/tuna:${env.PATH}"
 
+        sh "rm -rf SolverAnalytics"
         sh "git clone https://${FIN_TOKEN}:x-oauth-basic@github.com/ROCmSoftwarePlatform/SolverAnalytics.git"
         sh "touch __init__.py"
         sh "python3 ./SolverAnalytics/tests/clean_finddb_test.py"
