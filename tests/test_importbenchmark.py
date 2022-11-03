fa05cc0ca8ee966c01601f5aabaf2547fff69ebb
Alexandra Bara <barai@mymail.vcu.edu>
2022-11-03 16:07:04 -0500

    New script to operate on benchmarks/framework/model tables (#807)
    * added alembic
    
    * alembic file for new tables
    
    * added model as FKey to tags tabels
    
    * fixed mixin declaration
    
    * new file
    
    * made model nullable
    
    * made model and framework an FKey in the bechmark tables
    
    * 123
    
    * runing pylint on miopen folder + ignoring duplicate code for imports
    
    * fix
    
    * fixed order for table decl
    
    * Enum col needs at least 1 entry
    
    * 123
    
    * runing pylint on miopen folder + ignoring duplicate code for imports
    
    * test file for import_benchmark + updates
    
    * back to enum model
    
    * 123
    
    * typo
    
    * 123
    
    * update for new pylint constraints
    
    * yapf
    
    * pytest
    
    * call through name mangling
    
    Co-authored-by: Christopher Erb <christopher.erb@amd.com>

--- tests/test_importbenchmark.py
+++ tests/test_importbenchmark.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 ###############################################################################
 #
 # MIT License
@@ -24,51 +23,34 @@
 # SOFTWARE.
 #
 ###############################################################################
-""" Module for defining benchmark and model enums """
 
-import enum
-from sqlalchemy import Column, UniqueConstraint
-from sqlalchemy import Enum, Float
-from tuna.dbBase.base_class import BASE
+import os
+import sys
 
+sys.path.append("../tuna")
+sys.path.append("tuna")
 
-#pylint: disable=too-few-public-methods
-class FrameworkEnum(enum.Enum):
-  """Represents framework enums"""
-  PYTORCH = 'Pytorch'
-  DEEPBENCH = 'Deepbench'
+this_path = os.path.dirname(__file__)
 
-  def __str__(self):
-    return self.value
+from tuna.dbBase.sql_alchemy import DbSession
+from utils import DummyArgs
+from tuna.import_benchmark import add_model, update_frameworks, print_models
+from tuna.miopen.benchmark import Framework, ModelEnum
 
 
-class Framework(BASE):
-  """Represents framework table"""
-  __tablename__ = "framework"
-  __table_args__ = (UniqueConstraint("framework", name="uq_idx"),)
-  framework = Column(Enum(FrameworkEnum), nullable=False)
-
-
-class ModelEnum(enum.Enum):
-  """Represents model enums"""
-  RESNET50 = 'Resnet50'
-  RESNEXT101 = 'Resnext101'
-  VGG16 = 'Vgg16'
-  VGG19 = 'Vgg19'
-  ALEXNET = 'Alexnet'
-  GOOGLENET = 'Googlenet'
-  INCEPTION3 = 'Inception3'
-  INCEPTION4 = 'Inception4'
-  MASKRCNN = 'Mask-r-cnn'
-  SHUFFLENET = 'Shufflenet'
-
-  def __str__(self):
-    return self.value
-
-
-class Model(BASE):
-  """Represents model table"""
-  __tablename__ = "model"
-  __table_args__ = (UniqueConstraint("model", "version", name="uq_idx"),)
-  model = Column(Enum(ModelEnum), nullable=False)
-  version = Column(Float, nullable=False)
+def test_import_benchmark():
+  args = DummyArgs
+  models = {
+      ModelEnum.ALEXNET: 1.0,
+      ModelEnum.GOOGLENET: 2.0,
+      ModelEnum.VGG19: 3.0
+  }
+  for key, value in models.items():
+    args.add_model = key.value
+    args.version = value
+    add_model(args)
+  print_models()
+  update_frameworks()
+  with DbSession() as session:
+    frmks = session.query(Framework).all()
+    assert len(frmks) > 0
