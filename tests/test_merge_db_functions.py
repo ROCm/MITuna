5b968e2c9ef02a5023c75b9a9dfc7163753d8f2d
Sujatha Phatak <technicalgrp89@gmail.com>
2022-11-18 00:01:00 +0000

    PR - 815 	test cases are updated for review comments.

--- tests/test_merge_db_functions.py
+++ tests/test_merge_db_functions.py
@@ -27,17 +27,15 @@
 import sys
 import os
 import sqlite3
-import pdb
 
-from tuna.utils.merge_db import parse_jobline,parse_text_fdb_name,parse_text_pdb_name,load_master_list
-from tuna.utils.merge_db import best_solver,target_merge,no_job_merge,single_job_merge
-from tuna.utils.merge_db import multi_job_merge,update_master_list,write_merge_results
-from tuna.utils.merge_db import merge_text_file,merge_sqlite_pdb,merge_sqlite_bin_cache
-from tuna.utils.merge_db import merge_sqlite,get_file_list,get_sqlite_table
-from tuna.utils.merge_db import get_sqlite_row,get_sqlite_data
+from tuna.utils.merge_db import parse_jobline, parse_text_fdb_name, parse_text_pdb_name, load_master_list
+from tuna.utils.merge_db import best_solver, target_merge, no_job_merge, single_job_merge
+from tuna.utils.merge_db import multi_job_merge, update_master_list, write_merge_results
+from tuna.utils.merge_db import merge_text_file, merge_sqlite_pdb, merge_sqlite_bin_cache
+from tuna.utils.merge_db import merge_sqlite, get_file_list, get_sqlite_table
+from tuna.utils.merge_db import get_sqlite_row, get_sqlite_data
 from tuna.helper import prune_cfg_dims
 
-this_path = os.path.dirname(__file__)
 
 def test_parse_jobline():
 
@@ -45,25 +43,24 @@ def test_parse_jobline():
 
   key, vals = parse_jobline(data)
 
-  print('\n\n\n')
-  print(vals)
-  print('\n')
-  assert( key == '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F')
-  assert( vals == {'miopenConvolutionFwdAlgoImplicitGEMM':'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used','miopenConvolutionFwdAlgoWinograd': 'ConvBinWinogradRxSf2x3g1,0.03856,0,miopenConvolutionFwdAlgoWinograd,not used', 'miopenConvolutionFwdAlgoDirect': 'ConvOclDirectFwdGen,0.0536,0,miopenConvolutionFwdAlgoDirect,not used','miopenConvolutionFwdAlgoGEMM': 'GemmFwdRest,0.05712,2749200,miopenConvolutionFwdAlgoGEMM,not used'})
+  assert (key == '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F')
+  assert (vals == {
+      'miopenConvolutionFwdAlgoImplicitGEMM':
+          'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used',
+      'miopenConvolutionFwdAlgoWinograd':
+          'ConvBinWinogradRxSf2x3g1,0.03856,0,miopenConvolutionFwdAlgoWinograd,not used',
+      'miopenConvolutionFwdAlgoDirect':
+          'ConvOclDirectFwdGen,0.0536,0,miopenConvolutionFwdAlgoDirect,not used',
+      'miopenConvolutionFwdAlgoGEMM':
+          'GemmFwdRest,0.05712,2749200,miopenConvolutionFwdAlgoGEMM,not used'
+  })
 
- 
-  key = 'miopenConvolutionFwdAlgoImplicitGEMM'
+  algo = 'miopenConvolutionFwdAlgoImplicitGEMM'
+  assert (
+      vals[algo] ==
+      'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
+  )
 
-  print ('\n................')
-  print ( vals[key])
-  print('\n...........')
-
-  try :
-    assert( vals[key] == 'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used')
-  except AssertionError as msg:
-    print(msg)
-    raise  
- 
 
 def test_parse_text_fdb_name():
   master_file = '/data/tests/gfx90a_68.HIP.fdb.txt'
@@ -88,41 +85,49 @@ def test_parse_text_pdb_name():
 def test_load_master_list():
   master_file = '/data/tests/gfx803_36.HIP.fdb.txt'
   master_list = load_master_list(master_file)
-  
+
   list1 = list(master_list.keys())
   values = list(master_list.values())
-  
+
   if master_list == {}:
     assert False
   else:
     assert True
-  if master_list :
-    assert ( list(master_list.keys())[0] == '1-1-1-4x4-512-4-4-128-0x0-1x1-1x1-0-NCHW-FP32-B')
-    assert ( list(master_list.values())[0] ==  {'miopenConvolutionBwdDataAlgoWinograd': 'ConvBinWinogradRxSf2x3,0.06365,0,miopenConvolutionBwdDataAlgoWinograd,<unused>', 'miopenConvolutionBwdDataAlgoGEMM': 'GemmBwdRest,4.0393,32768,rocBlas,<unused>'})
+  if master_list:
+    assert (list(master_list.keys())[0] ==
+            '1-1-1-4x4-512-4-4-128-0x0-1x1-1x1-0-NCHW-FP32-B')
+    assert (list(master_list.values())[0] == {
+        'miopenConvolutionBwdDataAlgoWinograd':
+            'ConvBinWinogradRxSf2x3,0.06365,0,miopenConvolutionBwdDataAlgoWinograd,<unused>',
+        'miopenConvolutionBwdDataAlgoGEMM':
+            'GemmBwdRest,4.0393,32768,rocBlas,<unused>'
+    })
+
 
 def test_best_solver():
 
-  master_value = ({'miopenConvolutionFwdAlgoImplicitGEMM':
-  'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm, 0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used', 
-  'miopenConvolutionFwdAlgoWinograd': 'ConvBinWinogradRxSf2x3g1,0.03856,0,miopenConvolutionFwdAlgoWinograd,not used',
-  'miopenConvolutionFwdAlgoGEMM': 'GemmFwdRest,0.05712,2749200,miopenConvolutionFwdAlgoGEMM,not used'})
+  FdbKey_Set = ({
+      'miopenConvolutionFwdAlgoImplicitGEMM':
+          'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm, 0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used',
+      'miopenConvolutionFwdAlgoWinograd':
+          'ConvBinWinogradRxSf2x3g1,0.03856,0,miopenConvolutionFwdAlgoWinograd,not used',
+      'miopenConvolutionFwdAlgoGEMM':
+          'GemmFwdRest,0.05712,2749200,miopenConvolutionFwdAlgoGEMM,not used'
+  })
 
-  test_solver_value = ({'miopenConvolutionFwdAlgoImplicitGEMM': 
-  'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used '})
-
-  old_solver, old_time = best_solver(master_value)
-  new_solver, new_time = best_solver(test_solver_value)
-  if (len(new_solver) == 0):
+  solver, time = best_solver(FdbKey_Set)
+  if (len(solver) == 0):
     assert (False)
   else:
     assert (True)
 
-  if (float(new_time)):
+  if (float(time)):
     assert (True)
   else:
-    ssert (False)
-  
-  print( 'Old Solver: (%s, %s), New Solver: (%s, %s)',old_solver, old_time, new_solver,new_time)
+    assert (False)
+
+  assert (solver == 'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm')
+  assert (time == 0.02352)
 
   #Sample as space, time 0.0
   test_value = ({
@@ -133,17 +138,16 @@ def test_best_solver():
   new_solver, new_time = best_solver(test_value)
   if (not (new_solver and new_solver.isspace())):
     assert (True)
-  else :
+  else:
     assert (False)
 
   #Sample as None and time 0.0
 
   test_value = ({
       'miopenConvolutionFwdAlgoImplicitGEMM':
-      ' ,0.00000,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
-    })
+          ' ,0.00000,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
+  })
 
-  
   new_solver, new_time = best_solver(test_value)
   if (not (new_solver and new_solver == "None")):
     assert (False)
@@ -184,56 +188,81 @@ def test_target_merge():
   target_merge(master_list, key, vals, keep_keys)
 
   #new key with the avaliable values.
-  if ( key == '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F'):
-    assert(True)
-  if ( master_list[key] == {'miopenConvolutionFwdAlgoImplicitGEMM': 'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgo    ImplicitGEMM,not used'}):
-    assert(True)
+  if (key == '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F'):
+    assert (True)
+  if (master_list[key] == {
+      'miopenConvolutionFwdAlgoImplicitGEMM':
+          'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgo    ImplicitGEMM,not used'
+  }):
+    assert (True)
 
-  #test when we pass vals == {} 
+  keep_keys = True
+  key = '1-19-19-1x1-64-19-19-1024-0x0-1x1-1x1-0-NCHW-FP32-B'
+
+  target_merge(master_list, key, vals, keep_keys)
+
+  if ((master_list[key]) == {
+      'miopenConvolutionBwdDataAlgoWinograd':
+          'ConvBinWinogradRxSf2x3g1,0.186719,0,miopenConvolutionBwd    DataAlgoWinograd,not used',
+      'miopenConvolutionBwdDataAlgoDirect':
+          'ConvOclDirectFwd1x1,0.234239,0,miopenConvolutionBwdData    AlgoDirect,not used',
+      'miopenConvolutionBwdDataAlgoGEMM':
+          'GemmBwd1x1_stride1,0.239519,0,miopenConvolutionBwdDataAlgoGEMM,    not used',
+      'miopenConvolutionBwdDataAlgoImplicitGEMM':
+          'ConvAsmImplicitGemmGTCDynamicBwdXdlopsNHWC,0.327199,94633984,miope    nConvolutionBwdDataAlgoImplicitGEMM,not used',
+      'miopenConvolutionFwdAlgoImplicitGEMM':
+          'ConvMlirIgemmFwdXdlops,0.03776,0,m    iopenConvolutionFwdAlgoImplicitGEMM,not used'
+  }):
+
+    assert (True)
+
+  #test when we pass vals == {}
   keep_keys = False
 
   master_list = {
-    '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F': {
-        'miopenConvolutionFwdAlgoImplicitGEMM':
-            'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used',
-        'miopenConvolutionFwdAlgoWinograd':
-            'ConvBinWinogradRxSf2x3g1,0.03856,0,miopenConvolutionFwdAlgoWinograd,not used',
-        'miopenConvolutionFwdAlgoDirect':
-            'ConvOclDirectFwdGen,0.0536,0,miopenConvolutionFwdAlgoDirect,not used',
-        'miopenConvolutionFwdAlgoGEMM':
-            'GemmFwdRest,0.05712,2749200,miopenConvolutionFwdAlgoGEMM,not used'}}
+      '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F': {
+          'miopenConvolutionFwdAlgoImplicitGEMM':
+              'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02352,0,miopenConvolutionFwdAlgoImplicitGEMM,not used',
+          'miopenConvolutionFwdAlgoWinograd':
+              'ConvBinWinogradRxSf2x3g1,0.03856,0,miopenConvolutionFwdAlgoWinograd,not used',
+          'miopenConvolutionFwdAlgoDirect':
+              'ConvOclDirectFwdGen,0.0536,0,miopenConvolutionFwdAlgoDirect,not used',
+          'miopenConvolutionFwdAlgoGEMM':
+              'GemmFwdRest,0.05712,2749200,miopenConvolutionFwdAlgoGEMM,not used'
+      }
+  }
 
   key = '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F'
   vals = {}
-  target_merge(master_list,key,vals,keep_keys)
-  assert(vals == {})
+  target_merge(master_list, key, vals, keep_keys)
+  assert (vals == {})
 
-  try :
-    target_merge(master_list={},key={}, vals={}, keep_keys=False)
+  try:
+    target_merge(master_list={}, key={}, vals={}, keep_keys=False)
   except AttributeError:
-    assert(True)
+    assert (True)
 
-  try :
-    
-    keep_keys = False;
+  try:
+
+    keep_keys = False
     key = '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F'
 
     vals = {
-      'miopenConvolutionFwdAlgoImplicitGEMM':
-          'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
+        'miopenConvolutionFwdAlgoImplicitGEMM':
+            'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
     }
     master_list = {}
-    target_merge(master_list,key,vals,keep_keys)
+    target_merge(master_list, key, vals, keep_keys)
 
   except AttributeError:
-    assert(True)
+    assert (True)
 
-    keep_keys = False;
+    keep_keys = False
     key = '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F'
 
     vals = {
-      'miopenConvolutionFwdAlgoImplicitGEMM':
-          'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
+        'miopenConvolutionFwdAlgoImplicitGEMM':
+            'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
     }
 
 
@@ -259,7 +288,50 @@ def test_update_master_list():
   update_master_list(master_list, local_paths, mids, keep_keys)
 
   #master_list got updated for new entries
-  assert ( master_list == {'1-48-480-3x3-16-48-480-1-1x1-1x1-1x1-0-NCHW-FP16-F': {'miopenConvolutionFwdAlgoImplicitGEMM': 'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02512,0,miopenConvolutionFwdAlgoImplicitGEMM,not used', 'miopenConvolutionFwdAlgoDirect': 'ConvOclDirectFwd,0.02544,0,miopenConvolutionFwdAlgoDirect,not used', 'miopenConvolutionFwdAlgoWinograd': 'ConvBinWinogradRxSf2x3g1,0.03104,0,miopenConvolutionFwdAlgoWinograd,not used', 'miopenConvolutionFwdAlgoGEMM': 'GemmFwdRest,0.06096,414720,miopenConvolutionFwdAlgoGEMM,not used'}, '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F': {'miopenConvolutionFwdAlgoImplicitGEMM': 'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'}, '1-19-19-1x1-64-19-19-1024-0x0-1x1-1x1-0-NCHW-FP32-B': {'miopenConvolutionBwdDataAlgoDirect': 'ConvAsm1x1U,0.09728,0,miopenConvolutionBwdDataAlgoDirect,not used', 'miopenConvolutionBwdDataAlgoWinograd': 'ConvBinWinogradRxSf3x2,0.18192,0,miopenConvolutionBwdDataAlgoWinograd,not used', 'miopenConvolutionBwdDataAlgoImplicitGEMM': 'ConvMlirIgemmBwdXdlops,0.250079,0,miopenConvolutionBwdDataAlgoImplicitGEMM,not used', 'miopenConvolutionBwdDataAlgoGEMM': 'GemmBwd1x1_stride1,2732.22,0,miopenConvolutionBwdDataAlgoGEMM,not used'}, '1-19-19-1x1-64-19-19-1024-0x0-1x1-1x1-0-NCHW-FP32-W': {'miopenConvolutionBwdWeightsAlgoDirect': 'ConvAsmBwdWrW1x1,0.445761,0,miopenConvolutionBwdWeightsAlgoDirect,not used', 'miopenConvolutionBwdWeightsAlgoWinograd': 'ConvBinWinogradRxSf2x3,11.9268,0,miopenConvolutionBwdWeightsAlgoWinograd,not used', 'miopenConvolutionBwdWeightsAlgoImplicitGEMM': 'ConvMlirIgemmWrWXdlops,69.8604,0,miopenConvolutionBwdWeightsAlgoImplicitGEMM,not used'}, '1024-14-14-1x1-256-14-14-15-0x0-1x1-1x1-0-NCHW-FP16-F': {'miopenConvolutionFwdAlgoImplicitGEMM': 'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.03808,0,miopenConvolutionFwdAlgoImplicitGEMM,not used', 'miopenConvolutionFwdAlgoDirect': 'ConvAsm1x1U,0.11072,0,miopenConvolutionFwdAlgoDirect,not used', 'miopenConvolutionFwdAlgoWinograd': 'ConvBinWinogradRxSf3x2,0.208159,0,miopenConvolutionFwdAlgoWinograd,not used', 'miopenConvolutionFwdAlgoGEMM': 'GemmFwd1x1_0_1,2730.97,0,miopenConvolutionFwdAlgoGEMM,not used'}})
+  assert (master_list == {
+      '1-48-480-3x3-16-48-480-1-1x1-1x1-1x1-0-NCHW-FP16-F': {
+          'miopenConvolutionFwdAlgoImplicitGEMM':
+              'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.02512,0,miopenConvolutionFwdAlgoImplicitGEMM,not used',
+          'miopenConvolutionFwdAlgoDirect':
+              'ConvOclDirectFwd,0.02544,0,miopenConvolutionFwdAlgoDirect,not used',
+          'miopenConvolutionFwdAlgoWinograd':
+              'ConvBinWinogradRxSf2x3g1,0.03104,0,miopenConvolutionFwdAlgoWinograd,not used',
+          'miopenConvolutionFwdAlgoGEMM':
+              'GemmFwdRest,0.06096,414720,miopenConvolutionFwdAlgoGEMM,not used'
+      },
+      '1-160-698-5x5-64-79-348-1-1x1-2x2-1x1-0-NCHW-FP32-F': {
+          'miopenConvolutionFwdAlgoImplicitGEMM':
+              'ConvMlirIgemmFwdXdlops,0.03776,0,miopenConvolutionFwdAlgoImplicitGEMM,not used'
+      },
+      '1-19-19-1x1-64-19-19-1024-0x0-1x1-1x1-0-NCHW-FP32-B': {
+          'miopenConvolutionBwdDataAlgoDirect':
+              'ConvAsm1x1U,0.09728,0,miopenConvolutionBwdDataAlgoDirect,not used',
+          'miopenConvolutionBwdDataAlgoWinograd':
+              'ConvBinWinogradRxSf3x2,0.18192,0,miopenConvolutionBwdDataAlgoWinograd,not used',
+          'miopenConvolutionBwdDataAlgoImplicitGEMM':
+              'ConvMlirIgemmBwdXdlops,0.250079,0,miopenConvolutionBwdDataAlgoImplicitGEMM,not used',
+          'miopenConvolutionBwdDataAlgoGEMM':
+              'GemmBwd1x1_stride1,2732.22,0,miopenConvolutionBwdDataAlgoGEMM,not used'
+      },
+      '1-19-19-1x1-64-19-19-1024-0x0-1x1-1x1-0-NCHW-FP32-W': {
+          'miopenConvolutionBwdWeightsAlgoDirect':
+              'ConvAsmBwdWrW1x1,0.445761,0,miopenConvolutionBwdWeightsAlgoDirect,not used',
+          'miopenConvolutionBwdWeightsAlgoWinograd':
+              'ConvBinWinogradRxSf2x3,11.9268,0,miopenConvolutionBwdWeightsAlgoWinograd,not used',
+          'miopenConvolutionBwdWeightsAlgoImplicitGEMM':
+              'ConvMlirIgemmWrWXdlops,69.8604,0,miopenConvolutionBwdWeightsAlgoImplicitGEMM,not used'
+      },
+      '1024-14-14-1x1-256-14-14-15-0x0-1x1-1x1-0-NCHW-FP16-F': {
+          'miopenConvolutionFwdAlgoImplicitGEMM':
+              'ConvHipImplicitGemmForwardV4R4Xdlops_Padded_Gemm,0.03808,0,miopenConvolutionFwdAlgoImplicitGEMM,not used',
+          'miopenConvolutionFwdAlgoDirect':
+              'ConvAsm1x1U,0.11072,0,miopenConvolutionFwdAlgoDirect,not used',
+          'miopenConvolutionFwdAlgoWinograd':
+              'ConvBinWinogradRxSf3x2,0.208159,0,miopenConvolutionFwdAlgoWinograd,not used',
+          'miopenConvolutionFwdAlgoGEMM':
+              'GemmFwd1x1_0_1,2730.97,0,miopenConvolutionFwdAlgoGEMM,not used'
+      }
+  })
 
 
 def test_write_merge_results():
@@ -287,17 +359,13 @@ def test_write_merge_results():
     assert (True)
     f.close()
 
-  print("''''''''''''''''''''''''''''''''''''''''''''''''''''''\n\n")
   #input set to invalid types
   master_list = {}
 
   final_file = '/data/tests/final.txt'
-  key= {0}
+  key = {0}
   write_merge_results(master_list, final_file, copy_files)
-  
-  print(master_list)
-  print(final_file.content())
-  
+
   if key == open('/data/tests/final.txt').read():
     assert (True)
     f.close()
@@ -316,6 +384,12 @@ def test_merge_text_file():
   else:
     assert (False)
 
+  try:
+    assert [row for row in open(master_file)
+           ] == [row for row in open(target_file)]
+  except AssertionError as msg:
+    assert (True)
+
   #targetr_file set to None
 
   master_file = '/data/utils/test_files/old_gfx90a68.HIP.fdb.txt'
@@ -323,11 +397,12 @@ def test_merge_text_file():
                                 copy_only=False,
                                 keep_keys=False,
                                 target_file=None)
-                                
+
   if (os.stat(result_file)):
     assert (True)
   else:
-    assert(False)
+    assert (False)
+
 
 def test_get_sqlite_table():
 
@@ -339,6 +414,7 @@ def test_get_sqlite_table():
     assert (False)
   else:
     assert (True)
+
 
 def test_get_sqlite_row():
 
@@ -399,4 +475,3 @@ def test_get_sqlite_data():
     assert (False)
   else:
     assert (True)
-
