7871d4152a59f6f9ef190fe102abd73b20926b37
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-02-07 18:32:29 +0000

    omit

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -477,7 +477,7 @@ def pytestSuite1() {
            sh "python3 -m coverage run -a -m pytest tests/test_merge_db_functions.py -s"
            sh "python3 -m coverage run -a -m pytest tests/test_utility.py -s"
            sh "python3 -m coverage run -a -m pytest tests/test_example.py -s"
-           sh "coverage report --omit=*/usr/local/lib* "
+           sh "coverage report --omit=/usr/local/lib* "
            // The OBMC host used in the following test is down
            // sh "pytest tests/test_mmi.py "
         }
