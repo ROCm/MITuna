07d57faebe6381c97e856ff03f385a438d2ac3af
Sujatha <91573789+technicalgrp89@users.noreply.github.com>
2023-02-15 15:04:57 -0600

    Tuna corrupt configs - Type Annotation (#847)
    * -- type annotations enabled
             -- pylint formatting correction
             -- mypy command added into utils.groovy
    
    * mypy path corrections

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -543,6 +543,7 @@ def runLint() {
           def tuna_docker = docker.build("ci-tuna:${branch_id}", "--build-arg FIN_TOKEN=${FIN_TOKEN} .")
           tuna_docker.inside("") {
             sh "cd tuna && pylint -f parseable --max-args=8 --ignore-imports=no --indent-string='  ' *.py miopen/*.py example/*.py"
+            sh "cd tuna && mypy miopen/scripts/corrupt_configs.py --ignore-missing-imports --follow-imports=skip"
             sh "cd tuna && mypy miopen/utils/analyze_parse_db.py --ignore-missing-imports"
             sh "cd tuna && mypy miopen/scripts/build_driver_cmd.py --ignore-missing-imports --follow-imports=skip"
             sh "yamllint tuna/miopen/*.yaml"
