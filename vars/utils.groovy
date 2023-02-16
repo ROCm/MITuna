234e03f3c7a9cb31e27cc9e2f2d0cb54eb4229c0
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-02-16 21:49:44 +0000

    stage

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -533,7 +533,7 @@ def pytestSuite3() {
     }
 }
 
-def CoverageExport() {
+def coverageExport() {
     def tuna_docker = docker.build("ci-tuna:${branch_id}_pytest3", " --build-arg FIN_TOKEN=${FIN_TOKEN} --build-arg BACKEND=HIP .")
     tuna_docker.inside("--network host  --dns 8.8.8.8") {
         env.TUNA_DB_HOSTNAME = "${db_host}"
