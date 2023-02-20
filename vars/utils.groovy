de0feb256a5ef7103a4295138a1a7f7de1709b45
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-02-20 19:39:47 +0000

    test2

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -556,6 +556,7 @@ def coverageExport() {
            sh "python3 -m coverage json"
            sh "python3 tests/covscripts/coverage_develop.py"
            sh "ls"
+           sh "pwd"
         }
     }
 }
