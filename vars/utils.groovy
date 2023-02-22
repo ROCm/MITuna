30438776a6341975673b2b1a31d3b9088bc821da
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-02-22 17:00:47 +0000

    whoami

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -557,6 +557,7 @@ def coverageExport() {
            sh "python3 tests/covscripts/coverage_develop.py"
            sh "ls"
            sh "pwd"
+           sh "whoami"
            sh "sudo mv ./develop_percent_coverage.txt /home/fpadmin"
         }
     }
