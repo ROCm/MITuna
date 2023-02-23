e167494530b83007edc680cc34f9cb566e35c552
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-02-23 23:53:04 +0000

    curl

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -528,7 +528,7 @@ def pytestSuite3() {
            sh "python3 -m coverage run -a -m pytest tests/test_update_golden.py -s"
            sh "coverage report -m"
            sh "python3 -m coverage json"
-           sh "wget http://ginger.amd.com:8080/job/Test_mb/job/rk_coverage_auto/lastSuccessfulBuild/artifact/develop_percent_coverage.txt"
+           sh "curl -O http://ginger.amd.com:8080/job/Test_mb/job/rk_coverage_auto/lastSuccessfulBuild/artifact/develop_percent_coverage.txt"
            sh "python3 tests/covscripts/coverage_branches.py"
         }
     }
