c8c2b6e37ba9e39129eb99f0c55399b8eb81690e
Christopher Erb <chris.erb65@gmail.com>
2023-02-24 12:58:15 -0600

    add type hints

--- tuna/worker_interface.py
+++ tuna/worker_interface.py
@@ -29,7 +29,7 @@ from multiprocessing import Process, Lock
 try:
   import queue
 except ImportError:
-  import Queue as queue
+  import Queue as queue  #type: ignore
 import logging
 import os
 from datetime import datetime
@@ -39,6 +39,7 @@ import string
 from time import sleep
 from sqlalchemy.exc import IntegrityError, OperationalError, NoInspectionAvailable
 from sqlalchemy.inspection import inspect
+from typing import List, Tuple, Union
 
 from tuna.dbBase.sql_alchemy import DbSession
 from tuna.abort import chk_abort_file
@@ -152,7 +153,8 @@ class WorkerInterface(Process):
     self.machine.restart_server()
     self.last_reset = datetime.now()
 
-  def compose_work_objs(self, session, conds):
+  def compose_work_objs(self, session: DbSession,
+                        conds: List[str]) -> List[Tuple[SimpleDict, ...]]:
     """Query a job list for update"""
     cond_str = ' AND '.join(conds)
     if cond_str:
@@ -161,9 +163,10 @@ class WorkerInterface(Process):
     entries = gen_select_objs(session, self.job_attr,
                               self.dbt.job_table.__tablename__, cond_str)
 
-    return entries
+    return [(job) for job in entries]
 
-  def get_job_objs(self, session, find_state):
+  def get_job_objs(self, session: DbSession,
+                   find_state: str) -> List[Tuple[SimpleDict, ...]]:
     """Get list of job objects"""
     conds = [f"session={self.dbt.session.id}", "valid=1"]
 
@@ -297,7 +300,10 @@ class WorkerInterface(Process):
     return False
 
   # JD: This should take a session obj as an input to remove the creation of an extraneous session
-  def set_job_state(self, state, increment_retries=False, result=None):
+  def set_job_state(self,
+                    state: str,
+                    increment_retries: bool = False,
+                    result: Union[str, None] = None) -> None:
     """Interface function to update job state for builder/evaluator"""
     self.logger.info('Setting job id %s state to %s', self.job.id, state)
     with DbSession() as session:
