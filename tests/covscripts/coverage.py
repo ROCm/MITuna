ffeaa29f0f4684b2d0ac0c3dcf62af4b4b669b93
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-03-06 20:41:21 +0000

    parsearg/typ

--- tests/covscripts/coverage.py
+++ tests/covscripts/coverage.py
@@ -32,48 +32,55 @@ root_dir = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 file_path_json = os.path.join(root_dir, "./coverage.json")
 
+
 def develop():
-    """run and compare coverage for develop branch"""
-    coverage_file = open(file_path_json)
-    coverage_data = json.load(coverage_file)
-    percent_covered = coverage_data['totals']['percent_covered']
-    percent_covered = '{:.2f}'.format(percent_covered)
+  """run and compare coverage for develop branch"""
+  coverage_file = open(file_path_json)
+  coverage_data = json.load(coverage_file)
+  percent_covered = coverage_data['totals']['percent_covered']
+  percent_covered = '{:.2f}'.format(percent_covered)
 
-    print(f"Current Testing Coverage for develop branch is: {percent_covered}%")
+  print(f"Current Testing Coverage for develop branch is: {percent_covered}%")
 
-    with open('develop_percent_coverage.txt', 'w') as f:
-        f.write(percent_covered)
+  with open('develop_percent_coverage.txt', 'w') as f:
+    f.write(percent_covered)
 
-    check_export = os.path.exists('develop_percent_coverage.txt')
+  check_export = os.path.exists('develop_percent_coverage.txt')
 
-    if check_export is True:
-        print(f"Coverage artifact file is exported successfully")
-    else:
-        raise ValueError('Failed to save coverage artifacts')
+  if check_export is True:
+    print(f"Coverage artifact file is exported successfully")
+  else:
+    raise ValueError('Failed to save coverage artifacts')
+
 
 def branch():
-    """run and compare coverage for the non-develop, peripheral branches"""
-    file_path_prev = os.path.join(root_dir, "./develop_percent_coverage.txt")
-    coverage_file = open(file_path_json)
-    coverage_data = json.load(coverage_file)
-    percent_covered = coverage_data['totals']['percent_covered']
-    percent_covered = '{:.2f}'.format(percent_covered)
-    percent_covered = float(percent_covered)
+  """run and compare coverage for the non-develop, peripheral branches"""
+  file_path_prev = os.path.join(root_dir, "./develop_percent_coverage.txt")
+  coverage_file = open(file_path_json)
+  coverage_data = json.load(coverage_file)
+  percent_covered = coverage_data['totals']['percent_covered']
+  percent_covered = '{:.2f}'.format(percent_covered)
+  percent_covered = float(percent_covered)
 
-    with open(file_path_prev, 'r') as f:
-        prev_coverage = f.readline().strip()
-        prev_coverage_int = float(prev_coverage)
+  with open(file_path_prev, 'r') as f:
+    prev_coverage = f.readline().strip()
+    prev_coverage_int = float(prev_coverage)
 
-    print(f"Current Testing Coverage for current branch is: {percent_covered}%")
-    print(f"Current Testing Coverage for develop branch is: {prev_coverage_int}%")
-    if percent_covered < prev_coverage_int:
-        raise ValueError('Code Coverage Decreased')
-    
-parser = argparse.ArgumentParser(description='decide whether to extract coverage for either develop or peripheral branch')
-parser.add_argument('function', choices=['develop', 'branch'], help='choose the desired branch comb')
+  print(f"Current Testing Coverage for current branch is: {percent_covered}%")
+  print(f"Current Testing Coverage for develop branch is: {prev_coverage_int}%")
+  if percent_covered < prev_coverage_int:
+    raise ValueError('Code Coverage Decreased')
+
+
+parser = argparse.ArgumentParser(
+    description=
+    'decide whether to extract coverage for develop or a peripheral branch')
+parser.add_argument('function',
+                    choices=['develop', 'branch'],
+                    help='choose develop or branch')
 args = parser.parse_args()
 
 if args.function == 'develop':
-    develop()
+  develop()
 elif args.function == 'branch':
-    branch()
+  branch()
