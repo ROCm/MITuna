385b0d541b9b2a7471193eca178e4da22e1b2b04
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-03-07 21:51:32 +0000

    reform

--- tests/covscripts/coverage.py
+++ tests/covscripts/coverage.py
@@ -30,24 +30,41 @@ import argparse
 
 root_dir = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-file_path_json = os.path.join(root_dir, "./coverage.json")
+file_path_curcov_json = os.path.join(root_dir, "./coverage.json")
+file_path_prevcov_txt = os.path.join(root_dir, "./develop_percent_coverage.txt")
+
+
+def check_export(filename):
+  """check if the path was successfully exported"""
+  status = os.path.exists(filename)
+  return status
+
+
+def curcov():
+  """extract current coverage from a json file"""
+  curcov_json = open(file_path_curcov_json)
+  curcov_data = json.load(curcov_json)
+  curcov_percentage = curcov_data['totals']['percent_covered']
+  curcov_percentage_formated = '{:.2f}'.format(curcov_percentage)
+  curcov_percentage_ftdt = float(curcov_percentage_formated)
+  return curcov_percentage_ftdt
 
 
 def develop():
   """run and compare coverage for develop branch"""
-  coverage_file = open(file_path_json)
-  coverage_data = json.load(coverage_file)
-  percent_covered = coverage_data['totals']['percent_covered']
-  percent_covered = '{:.2f}'.format(percent_covered)
 
-  print(f"Current Testing Coverage for develop branch is: {percent_covered}%")
+  curcov_percentage_ftdt = curcov()
+
+  print(
+      f"Current Testing Coverage for develop branch is: {curcov_percentage_ftdt}%"
+  )
 
   with open('develop_percent_coverage.txt', 'w') as f:
-    f.write(percent_covered)
+    f.write(curcov_percentage_ftdt)
 
-  check_export = os.path.exists('develop_percent_coverage.txt')
+  check_txt_export = check_export('develop_percent_coverage.txt')
 
-  if check_export is True:
+  if check_txt_export is True:
     print(f"Coverage artifact file is exported successfully")
   else:
     raise ValueError('Failed to save coverage artifacts')
@@ -55,20 +72,20 @@ def develop():
 
 def branch():
   """run and compare coverage for the non-develop, peripheral branches"""
-  file_path_prev = os.path.join(root_dir, "./develop_percent_coverage.txt")
-  coverage_file = open(file_path_json)
-  coverage_data = json.load(coverage_file)
-  percent_covered = coverage_data['totals']['percent_covered']
-  percent_covered = '{:.2f}'.format(percent_covered)
-  percent_covered = float(percent_covered)
 
-  with open(file_path_prev, 'r') as f:
-    prev_coverage = f.readline().strip()
-    prev_coverage_int = float(prev_coverage)
+  curcov_percentage_ftdt = curcov()
 
-  print(f"Current Testing Coverage for current branch is: {percent_covered}%")
-  print(f"Current Testing Coverage for develop branch is: {prev_coverage_int}%")
-  if percent_covered < prev_coverage_int:
+  with open(file_path_prevcov_txt, 'r') as f:
+    prevcov_file = f.readline().strip()
+    prevcov_percentage_ftdt = float(prevcov_file)
+
+  print(
+      f"Current Testing Coverage for current branch is: {curcov_percentage_ftdt}%"
+  )
+  print(
+      f"Current Testing Coverage for develop branch is: {prevcov_percentage_ftdt}%"
+  )
+  if curcov_percentage_ftdt < prevcov_percentage_ftdt:
     raise ValueError('Code Coverage Decreased')
 
 
