e2ef8892f452dd0a7b29a8d040e97d6361131389
Christopher Erb <chris.erb65@gmail.com>
2023-03-14 13:28:07 -0500

    fix

--- tuna/miopen/worker/fin_class.py
+++ tuna/miopen/worker/fin_class.py
@@ -363,7 +363,7 @@ class FinClass(WorkerInterface):
 
           self.job_queue.put(master_cfg_list[start:end])
     try:
-      self.all_configs = self.job_queue.get(True, 180)
+      self.all_configs = self.job_queue.get(True, 30)
     except queue.Empty:
       self.logger.warning('No jobs found for process %s...', idx)
       self.all_configs = []
