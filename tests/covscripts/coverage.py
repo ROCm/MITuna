ce4945937b0feddf7a536906707f43fec564305f
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-03-17 14:34:39 -0400

    covscript

--- tests/covscripts/coverage.py
+++ tests/covscripts/coverage.py
@@ -1,110 +1,68 @@
-###############################################################################
-#
-# MIT License
-#
-# Copyright (c) 2023 Advanced Micro Devices, Inc.
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-###############################################################################
-
 import json
 import os
 import argparse
 
-COVERAGE_FILE_NAME_TXT = 'develop_percent_coverage.txt'
-COVERAGE_FILE_NAME_JSON = 'coverage.json'
-
-root_dir = os.path.dirname(__file__)
-file_path_curcov_json = os.path.join(root_dir, COVERAGE_FILE_NAME_JSON)
-file_path_prevcov_txt = os.path.join(root_dir, COVERAGE_FILE_NAME_TXT)
-
-
 def check_export(filename):
-  """check if the file was successfully exported"""
-  status = os.path.exists(filename)
-  return status
+    """check if the file was successfully exported"""
+    status = os.path.exists(filename)
+    return status
 
+def curcov(file_path_curcov_json):
+    """extract current coverage from a json file"""
+    with open(file_path_curcov_json) as curcov_json:
+        curcov_data = json.load(curcov_json)
+    curcov_percentage = curcov_data['totals']['percent_covered']
+    curcov_percentage_ftdt = round(curcov_percentage, 2)
+    return curcov_percentage_ftdt
 
-#make this main script function
-def curcov():
-  """extract current coverage from a json file"""
-  curcov_json = open(file_path_curcov_json)
-  curcov_data = json.load(curcov_json)
-  curcov_percentage = curcov_data['totals']['percent_covered']
-  curcov_percentage_formated = '{:.2f}'.format(curcov_percentage)
-  curcov_percentage_ftdt = float(curcov_percentage_formated)
-  return curcov_percentage_ftdt
+def develop(root_dir, coverage_file_name_txt, coverage_file_name_json):
+    """run and compare coverage for develop branch"""
+    file_path_curcov_json = os.path.join(root_dir, coverage_file_name_json)
+    curcov_percentage_ftdt = curcov(file_path_curcov_json)
 
+    print(f"Current Testing Coverage for develop branch is: {curcov_percentage_ftdt}%")
 
-def develop():
-  """run and compare coverage for develop branch"""
+    with open(coverage_file_name_txt, 'w') as f:
+        f.write(str(curcov_percentage_ftdt))
 
-  curcov_percentage_ftdt = curcov()
+    check_txt_export = check_export(coverage_file_name_txt)
 
-  print(
-      f"Current Testing Coverage for develop branch is: {curcov_percentage_ftdt}%"
-  )
+    if check_txt_export:
+        print(f'Coverage artifact file {coverage_file_name_txt} is exported successfully')
+    else:
+        raise ValueError(f'Failed to save coverage {coverage_file_name_txt} for artifact exports')
 
-  with open(COVERAGE_FILE_NAME_TXT, 'w') as f:
-    f.write(str(curcov_percentage_ftdt))
+def branch(root_dir, coverage_file_name_txt, coverage_file_name_json):
+    """run and compare coverage for the non-develop, peripheral branches"""
+    file_path_curcov_json = os.path.join(root_dir, coverage_file_name_json)
+    curcov_percentage_ftdt = curcov(file_path_curcov_json)
 
-  check_txt_export = check_export(COVERAGE_FILE_NAME_TXT)
+    with open(coverage_file_name_txt, 'r') as f:
+        prevcov_file = f.readline().strip()
+        prevcov_percentage_ftdt = float(prevcov_file)
 
-  if check_txt_export is True:
-    print(
-        f'Coverage artifact file {COVERAGE_FILE_NAME_TXT} is exported successfully'
-    )
-  else:
-    raise ValueError(
-        f'Failed to save coverage {COVERAGE_FILE_NAME_TXT} for artifact exports'
-    )
+    print(f"Current Testing Coverage for current branch is: {curcov_percentage_ftdt}%")
+    print(f"Current Testing Coverage for develop branch is: {prevcov_percentage_ftdt}%")
+    if curcov_percentage_ftdt < prevcov_percentage_ftdt:
+        raise ValueError('Code Coverage Decreased')
 
+def main():
+    root_dir = os.path.dirname(__file__)
 
-def branch():
-  """run and compare coverage for the non-develop, peripheral branches"""
+    parser = argparse.ArgumentParser(description='decide whether to extract coverage for develop or a peripheral branch')
+    parser.add_argument('function',
+                        choices=['develop', 'branch'],
+                        help='choose develop or branch')
+    parser.add_argument('--txt', dest='coverage_file_name_txt', default='develop_percent_coverage.txt',
+                        help='Name of the txt file to store the coverage percentage')
+    parser.add_argument('--json', dest='coverage_file_name_json', default='coverage.json',
+                        help='Name of the json file containing coverage data')
+    args = parser.parse_args()
 
-  curcov_percentage_ftdt = curcov()
+    if args.function == 'develop':
+        develop(root_dir, args.coverage_file_name_txt, args.coverage_file_name_json)
+    elif args.function == 'branch':
+        branch(root_dir, args.coverage_file_name_txt, args.coverage_file_name_json)
 
-  with open(COVERAGE_FILE_NAME_TXT, 'r') as f:
-    prevcov_file = f.readline().strip()
-    prevcov_percentage_ftdt = float(prevcov_file)
-
-  print(
-      f"Current Testing Coverage for current branch is: {curcov_percentage_ftdt}%"
-  )
-  print(
-      f"Current Testing Coverage for develop branch is: {prevcov_percentage_ftdt}%"
-  )
-  if curcov_percentage_ftdt < prevcov_percentage_ftdt:
-    raise ValueError('Code Coverage Decreased')
-
-
-parser = argparse.ArgumentParser(
-    description=
-    'decide whether to extract coverage for develop or a peripheral branch')
-parser.add_argument('function',
-                    choices=['develop', 'branch'],
-                    help='choose develop or branch')
-args = parser.parse_args()
-
-if args.function == 'develop':
-  develop()
-elif args.function == 'branch':
-  branch()
+if __name__ == '__main__':
+    main()
