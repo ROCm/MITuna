a5dae057d04a41be8af7ba07a25f4ff6b44b82bc
Christopher Erb <chris.erb65@gmail.com>
2023-04-06 10:23:30 -0500

    modify load_job filter

--- tuna/miopen/subcmd/load_job.py
+++ tuna/miopen/subcmd/load_job.py
@@ -285,7 +285,7 @@ def add_jobs(args, dbt):
     fin_step_str = 'not_fin'
     if args.fin_steps:
       fin_step_str = ','.join(args.fin_steps)
-    query = f"select config, solver from {dbt.job_table.__tablename__} where session={args.session_id} and fin_step='{fin_step_str}' and reason='{args.label}'"
+    query = f"select config, solver from {dbt.job_table.__tablename__} where session={args.session_id}"
     LOGGER.info(query)
     ret = session.execute(query)
     pre_ex = {}
