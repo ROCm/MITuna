a838d00c1bea8adfb7bec8f2f3a055c45518d93d
Alex Bara <barai@mymail.vcu.edu>
2023-04-11 14:28:00 -0500

    fixed utest

--- tests/test_yaml_parser.py
+++ tests/test_yaml_parser.py
@@ -181,7 +181,6 @@ def parse_example_yaml(example_yaml, example):
 
   dict1 = {
       'arch': 'gfx908',
-      'config_type': 'convolution',
       'docker_name': 'my_docker_name',
       'init_session': True,
       'label': 'Example',
@@ -192,7 +191,6 @@ def parse_example_yaml(example_yaml, example):
   }
   dict2 = {
       'arch': 'gfx908',
-      'config_type': 'convolution',
       'docker_name': 'my_docker_name',
       'execute': True,
       'label': 'Example',
