216d71273037fa659af07dbf1ff472416e515832
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-05-05 17:10:56 +0000

    testing_fdb + testing_pdb

--- tests/test_export_db.py
+++ tests/test_export_db.py
@@ -45,9 +45,13 @@ from unittest.mock import MagicMock
 
 from tuna.miopen.subcmd.export_db import arg_export_db, get_filename
 from tuna.miopen.subcmd.export_db import get_base_query, get_fdb_query
+from tuna.miopen.subcmd.export_db import get_pdb_query
 from tuna.utils.db_utility import get_id_solvers, DB_Type
-from tuna.miopen.db.tables import MIOpenDBTables
+from tuna.miopen.db.tables import MIOpenDBTables, ConfigType
 from tuna.miopen.parse_miopen_args import get_export_db_parser
+from tuna.dbBase.sql_alchemy import DbSession
+from tuna.miopen.db.find_db import ConvolutionFindDB
+from utils import add_test_session, DummyArgs
 
 
 @pytest.fixture
@@ -110,11 +114,49 @@ def test_get_base_query(mock_db_tables, mock_args, caplog):
   assert len(logs) > 0, "No logs must contains at least one record"
 
 
-"""
-def test_get_fdb_query(mock_db_tables, mock_args, caplog):
-    caplog.set_level(logging.INFO)
-    logger = logging.getLogger("test_logger")
-    query = get_fdb_query(mock_db_tables, mock_args, logger)
-    assert query is not None, "Query object is None"
-    assert isinstance(query, Query), f"epected query to be an instance of Query, Got {type(query)}"
-"""
+def build_fdb_entry(session_id):
+  fdb_entry = ConvolutionFindDB()
+  fdb_entry.config = 1
+  fdb_entry.solver = 1
+  fdb_entry.session = session_id
+  fdb_entry.opencl = False
+
+  fdb_entry.fdb_key = 'key'
+  fdb_entry.alg_lib = 'Test'
+  fdb_entry.params = 'param'
+  fdb_entry.workspace_sz = 0
+  fdb_entry.valid = True
+  fdb_entry.kernel_time = 11111
+  fdb_entry.kernel_group = 1
+
+  return fdb_entry
+
+
+def test_get_fdb_query(mock_args, caplog):
+  session_id = add_test_session()
+  fdb_entry = build_fdb_entry(session_id)
+  with DbSession() as session:
+    session.add(fdb_entry)
+    session.commit()
+
+  res = None
+  args = DummyArgs()
+  args.session_id = session_id
+  args.config_type = ConfigType.convolution
+  dbt = MIOpenDBTables(session_id=args.session_id, config_type=args.config_type)
+  caplog.set_level(logging.INFO)
+  logger = logging.getLogger("test_logger")
+
+  with DbSession() as session:
+    query_fdb = get_fdb_query(dbt, mock_args, logger)
+    query_pdb = get_pdb_query(dbt, mock_args, logger)
+    session.delete(fdb_entry)
+    session.commit()
+  assert query_fdb is not None, "Query object is None"
+  assert isinstance(
+      query_fdb,
+      Query), f"epected query to be an instance of Query, Got {type(query_fdb)}"
+  assert query_pdb is not None, "Query object is None"
+  assert isinstance(
+      query_pdb,
+      Query), f"epected query to be an instance of Query, Got {type(query_pdb)}"
