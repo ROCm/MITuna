2fc2c38bbd8a5cdb3a36862a2b83fd4819d6827a
Christopher Erb <chris.erb65@gmail.com>
2023-05-11 11:05:05 -0500

    export test

---
+++ tests/test_export_db.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+###############################################################################
+#
+# MIT License
+#
+# Copyright (c) 2022 Advanced Micro Devices, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+###############################################################################
+import sys
+import os
+
+from tuna.dbBase.sql_alchemy import DbSession
+from tuna.miopen.utils.config_type import ConfigType
+from tuna.miopen.db.tables import MIOpenDBTables
+from tuna.miopen.subcmd.export_db import get_fdb_query, build_miopen_fdb
+from utils import add_test_session, DummyArgs
+
+sys.path.append("../tuna")
+sys.path.append("tuna")
+
+this_path = os.path.dirname(__file__)
+
+def test_export_db():
+  session_id = add_test_session()
+
+  args = DummyArgs()
+  args.session_id = session_id
+  args.config_type = ConfigType.convolution
+
+  dbt = MIOpenDBTables(session_id=args.session_id)
+
+  if args.session_id:
+    args.arch = dbt.session.arch
+    args.num_cu = dbt.session.num_cu
+
+
+  with DbSession() as session:
+    fdb_entry = dbt.find_db_table()
+    fdb_entry.num_cu = 100
+    fdb_entry.config = 1
+    fdb_entry.arch = 'gfx100'
+    fdb_entry.opencl = False
+    fdb_entry.session = session_id
+    fdb_entry.fdb_key = '1x1'
+    session.add(fdb_entry)
+
+    fdb_entry2 = dbt.find_db_table()
+    fdb_entry2.num_cu = 100
+    fdb_entry2.config = 2
+    fdb_entry2.arch = 'gfx100'
+    fdb_entry2.opencl = False
+    fdb_entry2.session = session_id
+    fdb_entry2.fdb_key = '1x2'
+    session.add(fdb_entry2)
+
+    session.commit()
+
+  query = get_fdb_query(dbt, args)
+  miopen_fdb = build_miopen_fdb(query)
+
+  assert miopen_fdb['1x1'].config == 1
+  assert miopen_fdb['1x1'].num_cu == 100
+  assert miopen_fdb['1x1'].arch == 'gfx100'
+  assert miopen_fdb['1x2'].config == 2
+
