e677a43e86aa729ededab26ca117cb7969034904
Christopher Erb <chris.erb65@gmail.com>
2023-05-11 15:44:09 -0500

    fix test

--- tests/test_update_golden.py
+++ tests/test_update_golden.py
@@ -57,7 +57,7 @@ def build_fdb_entry(session_id):
 
 def test_update_golden():
   session_id = add_test_session(arch='gfx90a',
-                                num_cu=110,
+                                num_cu=104,
                                 label='pytest_update_golden')
   fdb_entry = build_fdb_entry(session_id)
   with DbSession() as session:
