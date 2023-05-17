2c03dd097180f5912f7e5ff09bae85c6c6d16adb
Christopher Erb <christopher.erb@amd.com>
2023-05-17 16:37:02 +0000

    pylint

--- tuna/miopen/driver/convolution.py
+++ tuna/miopen/driver/convolution.py
@@ -210,12 +210,12 @@ class DriverConvolution(DriverBase):
 
     if self.spatial_dim == 3 and len(self.out_layout) != 5:
       raise ValueError(
-          "Layout string (%s) should be 5 characters for spatial_dim %s",
-          self.out_layout, self.spatial_dim)
-    elif self.spatial_dim == 2 and len(self.out_layout) != 4:
+          f"Layout string {self.out_layout} should be 5 characters for spatial_dim {self.spatial_dim}"
+      )
+    if self.spatial_dim == 2 and len(self.out_layout) != 4:
       raise ValueError(
-          "Layout string (%s) should be 4 characters for spatial_dim %s",
-          self.out_layout, self.spatial_dim)
+          f"Layout string {self.out_layout} should be 4 characters for spatial_dim {self.spatial_dim}"
+      )
 
   @staticmethod
   def get_params(tok1: str) -> str:
