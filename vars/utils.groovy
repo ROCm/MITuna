4f1a1f0d0ecfcb2429bceb9579dde6180e48425d
Sujatha <91573789+technicalgrp89@users.noreply.github.com>
2023-06-12 17:16:51 -0500

    Sp/db base/base_class.py - typehint support (#907)
    * type hint support enabled
    
    * super init call added
    
    * __table__ definition added in BASE
    
    * review comments code change
    column defintion correction
    
    * __table__ definition added
      Dict[str, bool]->Dict[str,Any]

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -607,6 +607,7 @@ def runLint() {
             sh "mypy tuna/parse_args.py --ignore-missing-imports --follow-imports=skip"
             sh "mypy tuna/tables_interface.py --ignore-missing-imports --follow-imports=skip"
             sh "mypy tuna/sql.py --ignore-missing-imports"
+            sh "mypy tuna/dbBase/base_class.py --ignore-missing-imports"
           }
     }
 }
