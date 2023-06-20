23ed46624dc3f17c970b8981a1adfc4c46994f07
Sujatha Phatak <technicalgrp89@gmail.com>
2023-06-20 18:36:43 +0000

    type definitions update

--- tuna/example/example_tables.py
+++ tuna/example/example_tables.py
@@ -68,7 +68,7 @@ class Job(BASE):
   machine_id = Column(Integer, nullable=False, server_default="-1")
 
 
-def get_tables() -> List[str]:
+def get_tables() -> List[BASE]:
   """Returns a list of all Example lib DB tables"""
   tables: List[BASE] = []
   tables.append(SessionExample())
