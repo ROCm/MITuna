c75a5e6de603abf3748a8eebf86cfca112f1c743
Paul Fuqua <pf@acm.org>
2023-07-19 15:42:21 -0500

    First cut at tables for rocMLIR integration. Fix pylint warnings, add DBTables interface, first draft of RocMLIRWorker. Integrating rocmlir into the --add_tables and --init_session commands. Fix some table-connection and table-definition problems.  Now can run --add_tables   without needing build_schema.py first. Initial load_job.py. Implement config_string, import_configs.py, and run_cmd fetching configs from database. Changes to get --execute working.  Note the quoting of colons to avoid bind-params. Experiment with assigning workers to GPUs. Cleanup, reasons in the job table, compile-time trigger, results in the results table. Obtain kernel TFlops from tuningRunner.py for results table.  Handle result exceptions better.

--- tuna/miopen/subcmd/export_db.py
+++ tuna/miopen/subcmd/export_db.py
@@ -55,7 +55,6 @@ def require_id_solvers():
   if not ID_SOLVER_MAP:
     _, ID_SOLVER_MAP = get_id_solvers()
 
-
 def arg_export_db(args: argparse.Namespace, logger: logging.Logger):
   """export db args for exportdb"""
   if args.golden_v and not args.arch:
