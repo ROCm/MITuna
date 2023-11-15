189086bda15ed9b7fd1af765da7774169b3d9d68
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-11-15 23:13:11 +0000

    parse_fdb

--- tuna/miopen/driver/convolution.py
+++ tuna/miopen/driver/convolution.py
@@ -27,7 +27,6 @@
 """Module that a convolution MIOpenDriver cmd"""
 
 from typing import Dict, Set, Optional, Any
-from re import search
 from tuna.utils.logger import setup_logger
 from tuna.miopen.driver.base import MIOpenDriver
 from tuna.miopen.utils.metadata import CONV_CONFIG_COLS
@@ -36,8 +35,8 @@ from tuna.miopen.db.miopen_tables import ConvolutionConfig
 from tuna.miopen.utils.metadata import CONV_2D_DEFAULTS, SUPPORTED_CONV_CMDS, PREC_TO_CMD
 from tuna.miopen.utils.metadata import CONV_3D_DEFAULTS, TENSOR_COLS
 from tuna.miopen.utils.metadata import TABLE_COLS_CONV_MAP, TENSOR_PRECISION
-from tuna.miopen.utils.metadata import DIRECTION, DIR_MAP, CONV_SKIP_ARGS, INVERS_DIR_MAP
-from tuna.miopen.utils.parsing import get_fd_name, conv_arg_valid, get_fds_from_cmd
+from tuna.miopen.utils.metadata import DIRECTION, CONV_SKIP_ARGS, INVERS_DIR_MAP
+from tuna.miopen.utils.parsing import get_fd_name, conv_arg_valid
 from tuna.miopen.utils.config_type import ConfigType
 
 LOGGER = setup_logger('driver_conv')
@@ -118,20 +117,6 @@ class DriverConvolution(MIOpenDriver):
           f'Cannot instantiate convolution Driver class. Supported cmds are: {SUPPORTED_CONV_CMDS}'
       )
     self._cmd = value
-
-  def parse_fdb_key(self, line: str) -> None:
-    """import config attributes from fdb key line"""
-    fds: str
-    direction: str
-    fds, _, direction = get_fds_from_cmd(line)
-    setattr(self, 'direction', DIR_MAP[direction])
-    for key in self.to_dict():
-      if key in fds:
-        setattr(self, key, fds[key])
-
-    pattern_3d = '[0-9]x[0-9]x[0-9]'
-    if search(pattern_3d, line):
-      setattr(self, 'spatial_dim', 3)
 
   def parse_driver_line(self, line: str) -> None:
     """Parse MIOpenDriver line"""
