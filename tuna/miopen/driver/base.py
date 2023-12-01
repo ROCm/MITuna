11772839eb9161561aaac4cde3fbf15cdbe6508e
Reid Kawaja <74506315+reidkwja@users.noreply.github.com>
2023-12-01 22:59:03 +0000

    lint

--- tuna/miopen/driver/base.py
+++ tuna/miopen/driver/base.py
@@ -80,11 +80,6 @@ class MIOpenDriver(DriverBase):
     """Return operation layouts"""
     raise NotImplementedError("Not implemented")
 
-  @abstractmethod
-  def parse_fdb_key(self, line: str) -> None:
-    """Import config attributes from fdb key line"""
-    raise NotImplementedError("Not implemented")
-
   @staticmethod
   def test_skip_arg(tok1: str):
     """Check if token is skipable"""
