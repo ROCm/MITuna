ee1b6d7ab55c25aea03379e5504d1c8719c1a73c
Christopher Erb <Christopher.Erb@amd.com>
2024-01-12 14:25:20 -0600

    add celery launch to pipe compile command

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -890,7 +890,9 @@ def compile()
   }
 
   // Run the jobs on the cluster
-  sh "srun --no-kill -p ${partition} -N 1-10 -l bash -c 'docker run ${docker_args} ${tuna_docker_name} python3 /tuna/tuna/go_fish.py miopen ${compile_cmd} --session_id ${params.session_id}'"
+  sh "srun --no-kill -p ${partition} -N 1-10 -l bash -c \"docker run ${docker_args} ${tuna_docker_name}"
+        " sh -c 'celery -A tuna.celery_app.celery worker -l info -E --detach --logfile=/tmp/tunalogs -n celery_worker"
+        " && python3 /tuna/tuna/go_fish.py miopen ${compile_cmd} --session_id ${params.session_id}'\""
 }
 
 
