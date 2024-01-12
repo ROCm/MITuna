d866d0d987a71d8374c2adc3c6178f37c2a6c556
Sujatha Phatak <technicalgrp89@gmail.com>
2024-01-12 18:46:25 +0000

    pylint ignore introduced to handle W0102

--- tuna/utils/db_utility.py
+++ tuna/utils/db_utility.py
@@ -217,12 +217,10 @@ def get_class_by_tablename(tablename):
   return None
 
 
-def build_dict_val_key(obj: SimpleDict, exclude: List[str] = None):
+def build_dict_val_key(obj: SimpleDict, exclude: List[str] = ['id']):
+  #pylint: disable=dangerous-default-value
   """take object with to_dict function and create a key using values from the object's \
   sorted keys"""
-
-  exclude = ['id']
-
   obj_dict = obj.to_dict()
   for val in exclude:
     obj_dict.pop(val, False)
