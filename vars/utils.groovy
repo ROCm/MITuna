001209af53a77615f24f86c84c102e376cbff0b3
Christopher Erb <Christopher.Erb@amd.com>
2024-02-08 00:35:44 +0000

    docker login

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -51,7 +51,7 @@ def getDockerName(backend)
 }
 
 def buildDockers(){
-    docker.withRegistry("$docker_registry", 'tuna-dockerhub'){
+    docker.withRegistry('', 'tuna-dockerhub'){
         def tuna_docker_hipnogpu = docker.build(getDockerName("HIPNOGPU"), " --build-arg BACKEND=HIPNOGPU .")
         tuna_docker_hipnogpu.push()
         def tuna_docker_hip = docker.build(getDockerName("HIP"), " --build-arg BACKEND=HIP .")
