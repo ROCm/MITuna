211d70096742256afdd51b2cff973291488ea648
alecsandraBara <alexbara@amd.com>
2024-02-14 22:41:13 +0000

    utest fixes post code changes

--- tests/test_fin_evaluator.py
+++ tests/test_fin_evaluator.py
@@ -206,8 +206,8 @@ def test_fin_evaluator():
   with DbSession() as session:
     jobs = miopen.get_jobs(session, miopen.fetch_state, miopen.set_state,
                            miopen.args.session_id, 1)
-  job_entries = db_rows_to_obj(jobs, miopen.get_job_attr())
-  entries = [(job,) for job in job_entries]
+  #job_entries = db_rows_to_obj(jobs, miopen.get_job_attr())
+  entries = [job for job in job_entries]
   job_config_rows = miopen.compose_work_objs_fin(session, entries, miopen.dbt)
   assert (len(job_config_rows) == 80)
 
