b2a75abcace400097ec73b09621e7dd35a7c67b7
Christopher Erb <Christopher.Erb@amd.com>
2024-02-21 23:36:29 +0000

    test

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -873,6 +873,8 @@ def compile()
 
     // Run the jobs on the cluster
     withCredentials([usernamePassword(credentialsId: 'DOCKER_CRED', usernameVariable: 'UNAME', passwordVariable: 'PWORD')]){
+      sh 'echo $PWORD'
+      echo UNAME
       sh "srun --no-kill -p ${partition} -N 1-10 -l bash -c 'docker login -u ${UNAME} -p ${PWORD} && docker run ${docker_args} ${tuna_docker_name} python3 /tuna/tuna/go_fish.py miopen ${compile_cmd} --session_id ${params.session_id}'"
     }
   }
