6e6e53b37e11678e24576a03ac76ba18b3c41e76
Alex Bara <alex.bara@amd.com>
2024-07-08 21:35:39 +0000

    fixed groovy syntax

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -257,7 +257,7 @@ def finFindEval(){
             celery_log="${env.WORKSPACE}/tuna/${branch_id}_find_eval_celery_log_${counter}.log"
             sh "touch ${celery_log}"
             def proc_id = sh(script: "celery -A tuna.celery_app.celery_app worker -l info --logfile=${celery_log} -n tuna_${branch_id}_gpu_id_${counter} -Q eval_q_${db_name}_sess_${sesh1} -c 1 2>\0461 1>/dev/null & echo \$!", returnStdout: true).trim()
-            cat "${celery_log}"
+            sh "cat ${celery_log}"
             pid_list.add(proc_id)
             counter++
         }
