254f6fac8b330f9f303a6281414b2de388c0856e
Christopher Erb <Christopher.Erb@amd.com>
2024-07-25 21:23:20 +0000

    reduce logging

--- tuna/miopen/miopen_lib.py
+++ tuna/miopen/miopen_lib.py
@@ -720,7 +720,7 @@ class MIOpen(MITunaInterface):
   async def parse_result(self, data):
     """Function callback for celery async jobs to store results"""
     data = json.loads(data)
-    self.logger.info(data)
+    #self.logger.info(data)
 
     with DbSession() as session:
       try:
@@ -730,8 +730,8 @@ class MIOpen(MITunaInterface):
         self.logger.error(kerr)
         return False
 
-      self.logger.info('Parsing: %s', fin_json)
-      self.logger.info('Parsing context: %s', context)
+      #self.logger.info('Parsing: %s', fin_json)
+      #self.logger.info('Parsing context: %s', context)
       if self.operation == Operation.COMPILE:
         self.process_fin_builder_results(session, fin_json, context)
       elif self.operation == Operation.EVAL:
