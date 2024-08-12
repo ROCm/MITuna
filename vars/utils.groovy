249b1874b3aec4f6eee19010d99be596c63f8053
Alex Bara <alex.bara@amd.com>
2024-08-12 15:51:19 +0000

    more groovy syntax fix

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -456,7 +456,7 @@ def perfEval() {
         // &=046
         gpu_list.each{
             celery_log="${env.WORKSPACE}/tuna/${branch_id}_perf_eval_celery_log_${counter}.log"
-            celery_log_list.add(${celery_log})
+            celery_log_list.add(celery_log)
             sh "touch ${celery_log}"
             def proc_id = sh(script: "celery -A tuna.celery_app.celery_app worker -l info --logfile=${celery_log} -n tuna_${branch_id}_gpu_id_${counter} -Q eval_q_${db_name}_sess_${sesh1} -c 1 2>\0461 1>/dev/null & echo \$!", returnStdout: true).trim()
             sleep 5
@@ -496,7 +496,7 @@ def perfEval() {
 
         gpu_list.each{
             celery_log="${env.WORKSPACE}/tuna/${branch_id}_perf_eval_celery_log_${counter}.log"
-            celery_log_list.add(${celery_log})
+            celery_log_list.add(celery_log)
             sh "touch ${celery_log}"
             def proc_id = sh(script: "celery -A tuna.celery_app.celery_app worker -l info --logfile=${celery_log} -n tuna_${branch_id}_gpu_id_${counter} -Q eval_q_${db_name}_sess_${sesh1} -c 1 2>\0461 1>/dev/null & echo \$!", returnStdout: true).trim()
             sleep 5
