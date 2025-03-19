f682a10e1a212866b03998db0dd019a247f064cf
Christopher Erb <christopher.erb@amd.com>
2025-03-19 23:46:06 +0000

    fix

--- tuna/miopen/worker/fin_class.py
+++ tuna/miopen/worker/fin_class.py
@@ -647,7 +647,7 @@ class FinClass(WorkerInterface):
       result = {
         'solver': 'all',
         'success': False,
-        'result': out_str[-128:].replace('\n',';').replace('\'','"')
+        'result': out_str[-128:].replace('\n',';').replace('\'','"').replace('%','x')  # correct string for sql
       }
       return result
 
