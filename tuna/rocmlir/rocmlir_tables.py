7710f35470e1adfcee99dc17d115c4f3cafdeb7b
Daniel Hernandez-Juarez <dhernandez0@gmail.com>
2025-12-03 14:55:39 +0100

    Set default value of causal, return_lse, with-attn-scale and with-attn-bias to false (#1015)

--- tuna/rocmlir/rocmlir_tables.py
+++ tuna/rocmlir/rocmlir_tables.py
@@ -742,7 +742,7 @@ class AttentionConfig(BASE, SimpleCSVMixin):
       for datatype, transQ, transK, transV, transO, withAttnScale, withAttnBias, causal, return_lse, line in \
               itertools.product(['f32', 'f16'], ['false', 'true'],
                                 ['false', 'true'], ['false', 'true'],
-                                ['false', 'true'], ['false', 'true'], ['false', 'true'], ['false', 'true'], ['false', 'true'], lines):
+                                ['false', 'true'], ['false'], ['false'], ['false'], ['false'], lines):
         line = line.strip()
 
         # Skip empty lines
